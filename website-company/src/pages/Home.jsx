8f60e2db632c52d7bbd5fea8148546411be0acf1
ThalesFortes <thales_pro@outlook.com>
2025-07-23 21:46:02 -0300

    Create banners

--- website-company/src/pages/Home.jsx
+++ website-company/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Header from "../components/Header/Header"
 import Footer  from "../components/Footer/Footer"
 import Button from "../components/Button/Button"
+import Banner from "../components/Banner/Banner"
 
 function Home () {
   return(
@@ -9,6 +10,7 @@ function Home () {
       <h1>Home</h1>
       <p>Subtitle</p>
       <Button buttonStyle="primary" arrow>Olá</Button>
+      <Banner title="Home" image="about.jpg"/>
       <Footer/>
     </>
   )
