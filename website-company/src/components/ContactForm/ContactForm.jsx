6f190e32a4294f5994097b0d1b57edf67d169b1f
ThalesFortes <thales_pro@outlook.com>
2025-07-24 18:28:14 -0300

    Adjusting pages

--- website-company/src/components/ContactForm/ContactForm.jsx
+++ website-company/src/components/ContactForm/ContactForm.jsx
@@ -3,7 +3,7 @@ import './ContactForm.css'
 function ContactForm (props) {
   return(
     <div className='contact-form d-flex fd-column al-center'>
-        <h2>We love meeting new peopple</h2>
+        <h2>We love meeting new people and helping them.</h2>
         <form action="">WTP</form>
     </div>
   )
