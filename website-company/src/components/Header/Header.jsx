d7c77e7cd1688db501f6963e36ad61d0e2b0a95c
ThalesFortes <thales_pro@outlook.com>
2025-07-24 22:48:48 -0300

    hook useState

--- website-company/src/components/Header/Header.jsx
+++ website-company/src/components/Header/Header.jsx
@@ -1,17 +1,26 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 import Logo from "../../assets/logo-enterprise.svg"
 import "./Header.css"
 
+// COMPONENTS
+import Button from "../Button/Button"
 
 function Header () {
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleMenu = () => setIsOpen(!isOpen)
   return(
     <>
       <header>
         <div className="container">
           <div className="al-center d-flex jc-space-between">
             <Link to="/"><img src={Logo} alt="logo da empresa dnc" /></Link>
-            <nav>
+            <div className="mobile-menu">
+              <Button buttonStyle="secondary" onClick={()=> toggleMenu()}>Menu</Button>
+            </div>
+            <nav className={`${isOpen ? "open": " "} `}>
+              <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={()=> toggleMenu()}>X</Button>
               <ul className="d-flex">
                 <li><Link to="/">Home</Link></li>
                 <li><Link to="/about">About</Link></li>
