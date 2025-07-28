06a2c2972c61a6293468ed17fe0b501f81132e5c
ThalesFortes <thales_pro@outlook.com>
2025-07-28 16:17:34 -0300

    service api get data projects

--- website-company/src/components/ProjectsList/ProjectsList.jsx
+++ website-company/src/components/ProjectsList/ProjectsList.jsx
@@ -1,10 +1,29 @@
+import { useState , useEffect } from 'react'
 import './ProjectsList.css'
 
 // ASSETS
 import LikedFilled from "../../assets/liked-icon.svg"
 import Like from "../../assets/lik-icon.svg"
 
+// UTILS
+import { getApiData } from '../../services/apiServices'
+
 function ProjectsList (props) {
+  const [projects, setProjects] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const projectsResponse = await getApiData('projects')
+        setProjects(projectsResponse)
+      } catch {
+        setProjects([])
+      }
+    }
+
+    fetchData()
+  }, [])
+
   return(
     <div className='projects-section'>
       <div className='projects-hero'>
@@ -12,54 +31,16 @@ function ProjectsList (props) {
         <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
       </div>
       <div className='projects-grid'>
-        <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-         <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-         <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-        <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-         <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-         <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-         <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
-         <div className='project-card d-flex jc-center al-center fd-column'>
-          <div className='thumb tertiary-background'></div>
-          <h3>João Silva</h3>
-          <p>BH, Brasil</p>
-          <img src={LikedFilled} />
-        </div>
+        {projects.map((project) =>  (
+          <div key={project.id} className='project-card d-flex jc-center al-center fd-column'>
+            <div 
+              className='thumb tertiary-background'
+              style={{backgroundImage:`url(${project.thumb})`}}></div>
+              <h3>{project.title}</h3>
+              <p>{project.subtitle}</p>
+              <img src={LikedFilled} height="20px"/>
+          </div>
+        ))}
       </div>
     </div>
   )
