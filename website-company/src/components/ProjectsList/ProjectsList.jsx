4f93dd62d1265456f5be1a3828b833848195bded
ThalesFortes <thales_pro@outlook.com>
2025-07-28 20:28:43 -0300

    Validation forms

--- website-company/src/components/ProjectsList/ProjectsList.jsx
+++ website-company/src/components/ProjectsList/ProjectsList.jsx
@@ -31,16 +31,21 @@ function ProjectsList (props) {
         <p>It is a long established fact that a reader will be distracted by the of readable content of page  lookings at its layouts  points.</p>
       </div>
       <div className='projects-grid'>
-        {projects.map((project) =>  (
-          <div key={project.id} className='project-card d-flex jc-center al-center fd-column'>
-            <div 
-              className='thumb tertiary-background'
-              style={{backgroundImage:`url(${project.thumb})`}}></div>
-              <h3>{project.title}</h3>
-              <p>{project.subtitle}</p>
-              <img src={LikedFilled} height="20px"/>
-          </div>
-        ))}
+        { 
+          projects ?
+            projects.map((project) =>  (
+              <div key={project.id} className='project-card d-flex jc-center al-center fd-column'>
+                <div 
+                  className='thumb tertiary-background'
+                  style={{backgroundImage:`url(${project.thumb})`}}></div>
+                  <h3>{project.title}</h3>
+                  <p>{project.subtitle}</p>
+                  <img src={LikedFilled} height="20px"/>
+              </div>
+            ))
+         : 
+          null
+        }
       </div>
     </div>
   )
